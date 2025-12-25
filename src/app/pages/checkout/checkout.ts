f8682b54e57f129795c634945a49a4993357b892
migel <migelangelo10@gmail.com>
2025-12-25 11:51:24 +0200

    phone control completed

--- src/app/pages/checkout/checkout.ts
+++ src/app/pages/checkout/checkout.ts
@@ -1,11 +1,152 @@
 import { Component } from '@angular/core';
+import {AddToCart} from '../../services/add-to-cart';
+import {ProductType} from '../../product/product-type';
+import {ScrollTotopService} from '../../services/scrolltotop-service';
+import {RouterLink} from '@angular/router';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {CustomValidators} from '../../validators/validators';
+import {Phones, PhoneType} from '../../phones/phones-types';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule
+  ],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
 export class Checkout {
 
+  public cartItems: ProductType[] = [];
+  public labelActive: boolean = false;
+  public labelFirstNameActive: boolean = false;
+  public labelLastNameActive: boolean = false;
+  public labelCompanyActive: boolean = false;
+  public labelShippingAddressActive: boolean = false;
+  public labelApartmentActive: boolean = false;
+  public labelCityActive: boolean = false;
+  public labelZipcodeActive: boolean = false;
+  public labelPostalcodeActive: boolean = false;
+  public labelPhoneActive: boolean = false;
+  public shipToAddressChecked: boolean = true;
+  public shipToPickupPointChecked: boolean = false;
+  public currentCountry: string = 'US';
+
+  public phones: PhoneType[] = Phones;
+  public currentPhoneFlag: string = this.phones[0].flag;
+
+  public formCheckoutGroup: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.email]),
+    checkBox: new FormControl(true),
+    delivery_strategies: new FormControl('delivery-to-address'),
+    country_select: new FormControl('US'),
+    first_name: new FormControl('', [Validators.required]),
+    last_name: new FormControl('', [Validators.required]),
+    company: new FormControl(''),
+    shipping_address: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    apartment: new FormControl(''),
+    city: new FormControl('', [Validators.required]),
+    state_select: new FormControl(''),
+    zip_code: new FormControl('', [Validators.required]),
+    postal_code: new FormControl(''),
+    phone: new FormControl('', [Validators.required, CustomValidators.phoneValidator]),
+    phone_country_select: new FormControl(''),
+  });
+
+  constructor(private  cartService: AddToCart, private scrollTotopService: ScrollTotopService) {
+  }
+
+  ngOnInit() {
+    this.scrollTotopService.toTop();
+    this.cartItems = this.cartService.getCart();
+
+    console.log(this.cartItems);
+  }
+
+  get email() { return this.formCheckoutGroup.get('email'); }
+  get firstName() { return this.formCheckoutGroup.get('first_name'); }
+  get lastName() { return this.formCheckoutGroup.get('last_name'); }
+  get shippingAddress() { return this.formCheckoutGroup.get('shipping_address'); }
+  get city() { return this.formCheckoutGroup.get('city'); }
+  get zipcode() { return this.formCheckoutGroup.get('zip_code'); }
+  get phone() { return this.formCheckoutGroup.get('phone'); }
+
+  public onInputChange(): void {
+    this.labelActive = this.formCheckoutGroup.value.email.length > 0;
+  }
+  public onInputCountryChange(): void {
+    this.currentCountry = this.formCheckoutGroup.value.country_select;
+  }
+  public onInputFirstNameChange(): void {
+    this.labelFirstNameActive = this.formCheckoutGroup.value.first_name.length > 0;
+  }
+  public onInputLastNameChange(): void {
+    this.labelLastNameActive = this.formCheckoutGroup.value.last_name.length > 0;
+  }
+  public onInputCompanyChange(): void {
+    this.labelCompanyActive = this.formCheckoutGroup.value.company.length > 0;
+  }
+  public onInputShippingAddressChange(): void {
+    this.labelShippingAddressActive = this.formCheckoutGroup.value.shipping_address.length > 0;
+  }
+  public onInputShippingApartmentChange(): void {
+    this.labelApartmentActive = this.formCheckoutGroup.value.apartment.length > 0;
+  }
+  public onInputShippingCityChange(): void {
+    this.labelCityActive = this.formCheckoutGroup.value.city.length > 0;
+  }
+  public onInputZipcodeChange(): void {
+    this.labelZipcodeActive = this.formCheckoutGroup.value.zip_code.length > 0;
+  }
+  public onInputPostalodeChange(): void {
+    this.labelPostalcodeActive = this.formCheckoutGroup.value.postal_code.length > 0;
+  }
+  public onInputPhoneChange(): void {
+    this.labelPhoneActive = this.formCheckoutGroup.value.phone.length > 0;
+  }
+  public onInputPhoneCountryChange(): void {
+
+    let code: string | null = this.getPhoneCode(this.formCheckoutGroup.value.phone_country_select);
+    let flag: string | null = this.getPhoneFlag(this.formCheckoutGroup.value.phone_country_select);
+
+    if (code !== null) {
+      let value: string = code + " ";
+
+      this.formCheckoutGroup.patchValue({
+        phone: value
+      });
+    }
+
+    if (flag !== null) {
+      this.currentPhoneFlag = flag;
+    }
+  }
+
+  public shipToAddressClick(): void {
+    this.shipToAddressChecked = true;
+    this.shipToPickupPointChecked = false;
+  }
+  public shipToPickUpPointClick(): void {
+    this.shipToAddressChecked = false;
+    this.shipToPickupPointChecked = true;
+  }
+  private getPhoneCode(id: string): string | null {
+
+    for (let i = 0; i < this.phones.length; i++) {
+      if (id === this.phones[i].id) {
+        return this.phones[i].code;
+      }
+    }
+    return null;
+  }
+  private getPhoneFlag(id: string): string | null {
+
+    for (let i = 0; i < this.phones.length; i++) {
+      if (id === this.phones[i].id) {
+        return this.phones[i].flag;
+      }
+    }
+    return null;
+  }
 }
---
+++ src/app/phones/phones-types.ts
@@ -0,0 +1,1192 @@
+export interface PhoneType {
+  id: string;
+  flag: string;
+  code: string;
+}
+export const Phones: PhoneType[] = [
+  {
+    id: 'US',
+    flag: '/assets/images/us-flag.svg',
+    code: '+1',
+  },
+  {
+    id: 'AF',
+    flag: '/assets/images/af.svg',
+    code: '+93',
+  },
+  {
+    id: 'AX',
+    flag: '/assets/images/finland-aaland.svg',
+    code: '+358',
+  },
+  {
+    id: 'AL',
+    flag: '/assets/images/al.svg',
+    code: '+355',
+  },
+  {
+    id: 'DZ',
+    flag: '/assets/images/dz.svg',
+    code: '+213',
+  },
+  {
+    id: 'AD',
+    flag: '/assets/images/ad.svg',
+    code: '+376',
+  },
+  {
+    id: 'AO',
+    flag: '/assets/images/ao.svg',
+    code: '+244',
+  },
+  {
+    id: 'AI',
+    flag: '/assets/images/ai.svg',
+    code: '+1',
+  },
+  {
+    id: 'AG',
+    flag: '/assets/images/ag.svg',
+    code: '+1',
+  },
+  {
+    id: 'AR',
+    flag: '/assets/images/ar.svg',
+    code: '+54',
+  },
+  {
+    id: 'AM',
+    flag: '/assets/images/am.svg',
+    code: '+374',
+  },
+  {
+    id: 'AW',
+    flag: '/assets/images/aw.svg',
+    code: '+297',
+  },
+  {
+    id: 'AC',
+    flag: '/assets/images/ac.svg',
+    code: '+247',
+  },
+  {
+    id: 'AU',
+    flag: '/assets/images/au.svg',
+    code: '+61',
+  },
+  {
+    id: 'AT',
+    flag: '/assets/images/at.svg',
+    code: '+43',
+  },
+  {
+    id: 'AZ',
+    flag: '/assets/images/az.svg',
+    code: '+994',
+  },
+  {
+    id: 'BS',
+    flag: '/assets/images/bs.svg',
+    code: '+1',
+  },
+  {
+    id: 'BH',
+    flag: '/assets/images/bh.svg',
+    code: '+973',
+  },
+  {
+    id: 'BD',
+    flag: '/assets/images/bd.svg',
+    code: '+880',
+  },
+  {
+    id: 'BB',
+    flag: '/assets/images/bb.svg',
+    code: '+1',
+  },
+  {
+    id: 'BY',
+    flag: '/assets/images/by.svg',
+    code: '+375',
+  },
+  {
+    id: 'BE',
+    flag: '/assets/images/be.svg',
+    code: '+32',
+  },
+  {
+    id: 'BZ',
+    flag: '/assets/images/bz.svg',
+    code: '+501',
+  },
+  {
+    id: 'BJ',
+    flag: '/assets/images/bj.svg',
+    code: '+229',
+  },
+  {
+    id: 'BM',
+    flag: '/assets/images/bm.svg',
+    code: '+1',
+  },
+  {
+    id: 'BT',
+    flag: '/assets/images/bt.svg',
+    code: '+975',
+  },
+  {
+    id: 'BO',
+    flag: '/assets/images/bo.svg',
+    code: '+591',
+  },
+  {
+    id: 'BA',
+    flag: '/assets/images/ba.svg',
+    code: '+387',
+  },
+  {
+    id: 'BW',
+    flag: '/assets/images/bw.svg',
+    code: '+267',
+  },
+  {
+    id: 'BR',
+    flag: '/assets/images/br.svg',
+    code: '+55',
+  },
+  {
+    id: 'IO',
+    flag: '/assets/images/io.svg',
+    code: '+246',
+  },
+  {
+    id: 'VG',
+    flag: '/assets/images/vg.svg',
+    code: '+1',
+  },
+  {
+    id: 'BN',
+    flag: '/assets/images/bn.svg',
+    code: '+673',
+  },
+  {
+    id: 'BG',
+    flag: '/assets/images/bg.svg',
+    code: '+359',
+  },
+  {
+    id: 'BF',
+    flag: '/assets/images/bf.svg',
+    code: '+226',
+  },
+  {
+    id: 'BI',
+    flag: '/assets/images/bi.svg',
+    code: '+257',
+  },
+  {
+    id: 'KH',
+    flag: '/assets/images/kh.svg',
+    code: '+855',
+  },
+  {
+    id: 'CM',
+    flag: '/assets/images/cm.svg',
+    code: '+237',
+  },
+  {
+    id: 'CA',
+    flag: '/assets/images/ca.svg',
+    code: '+1',
+  },
+  {
+    id: 'CV',
+    flag: '/assets/images/cv.svg',
+    code: '+238',
+  },
+  {
+    id: 'BQ',
+    flag: '/assets/images/cw.svg',
+    code: '+599',
+  },
+  {
+    id: 'KY',
+    flag: '/assets/images/ky.svg',
+    code: '+1',
+  },
+  {
+    id: 'CF',
+    flag: '/assets/images/cf.svg',
+    code: '+236',
+  },
+  {
+    id: 'TD',
+    flag: '/assets/images/td.svg',
+    code: '+235',
+  },
+  {
+    id: 'CL',
+    flag: '/assets/images/cl.svg',
+    code: '+56',
+  },
+  {
+    id: 'CN',
+    flag: '/assets/images/cn.svg',
+    code: '+86',
+  },
+  {
+    id: 'CX',
+    flag: '/assets/images/au.svg',
+    code: '+61',
+  },
+  {
+    id: 'CC',
+    flag: '/assets/images/cc.svg',
+    code: '+891',
+  },
+  {
+    id: 'CO',
+    flag: '/assets/images/co.svg',
+    code: '+57',
+  },
+  {
+    id: 'KM',
+    flag: '/assets/images/km.svg',
+    code: '+269',
+  },
+  {
+    id: 'CG',
+    flag: '/assets/images/cg.svg',
+    code: '+242',
+  },
+  {
+    id: 'CD',
+    flag: '/assets/images/cd.svg',
+    code: '+243',
+  },
+  {
+    id: 'CK',
+    flag: '/assets/images/ck.svg',
+    code: '+682',
+  },
+  {
+    id: 'CR',
+    flag: '/assets/images/cr.svg',
+    code: '+506',
+  },
+  {
+    id: 'HR',
+    flag: '/assets/images/hr.svg',
+    code: '+385',
+  },
+  {
+    id: 'CW',
+    flag: '/assets/images/cw.svg',
+    code: '+599',
+  },
+  {
+    id: 'CY',
+    flag: '/assets/images/cy.svg',
+    code: '+357',
+  },
+  {
+    id: 'CZ',
+    flag: '/assets/images/cz.svg',
+    code: '+420',
+  },
+  {
+    id: 'CI',
+    flag: '/assets/images/ci.svg',
+    code: '+225',
+  },
+  {
+    id: 'DK',
+    flag: '/assets/images/dk.svg',
+    code: '+45',
+  },
+  {
+    id: 'DJ',
+    flag: '/assets/images/dj.svg',
+    code: '+253',
+  },
+  {
+    id: 'DM',
+    flag: '/assets/images/dm.svg',
+    code: '+1',
+  },
+  {
+    id: 'DO',
+    flag: '/assets/images/do.svg',
+    code: '+1',
+  },
+  {
+    id: 'EC',
+    flag: '/assets/images/ec.svg',
+    code: '+593',
+  },
+  {
+    id: 'EG',
+    flag: '/assets/images/eg.svg',
+    code: '+20',
+  },
+  {
+    id: 'SV',
+    flag: '/assets/images/sv.svg',
+    code: '+503',
+  },
+  {
+    id: 'GQ',
+    flag: '/assets/images/gq.svg',
+    code: '+240',
+  },
+  {
+    id: 'ER',
+    flag: '/assets/images/er.svg',
+    code: '+291',
+  },
+  {
+    id: 'EE',
+    flag: '/assets/images/ee.svg',
+    code: '+372',
+  },
+  {
+    id: 'SZ',
+    flag: '/assets/images/sz.svg',
+    code: '+268',
+  },
+  {
+    id: 'ET',
+    flag: '/assets/images/et.svg',
+    code: '+251',
+  },
+  {
+    id: 'FK',
+    flag: '/assets/images/fk.svg',
+    code: '+500',
+  },
+  {
+    id: 'FO',
+    flag: '/assets/images/fo.svg',
+    code: '+298',
+  },
+  {
+    id: 'FJ',
+    flag: '/assets/images/fj.svg',
+    code: '+679',
+  },
+  {
+    id: 'FI',
+    flag: '/assets/images/fi.svg',
+    code: '+358',
+  },
+  {
+    id: 'FR',
+    flag: '/assets/images/fr.svg',
+    code: '+33',
+  },
+  {
+    id: 'GF',
+    flag: '/assets/images/gf.svg',
+    code: '+594',
+  },
+  {
+    id: 'PF',
+    flag: '/assets/images/pf.svg',
+    code: '+689',
+  },
+  {
+    id: 'TF',
+    flag: '/assets/images/tf.svg',
+    code: '+262',
+  },
+  {
+    id: 'GA',
+    flag: '/assets/images/ga.svg',
+    code: '+241',
+  },
+  {
+    id: 'GM',
+    flag: '/assets/images/gm.svg',
+    code: '+220',
+  },
+  {
+    id: 'GE',
+    flag: '/assets/images/ge.svg',
+    code: '+995',
+  },
+  {
+    id: 'DE',
+    flag: '/assets/images/de.svg',
+    code: '+49',
+  },
+  {
+    id: 'GH',
+    flag: '/assets/images/gh.svg',
+    code: '+233',
+  },
+  {
+    id: 'GI',
+    flag: '/assets/images/gi.svg',
+    code: '+350',
+  },
+  {
+    id: 'GR',
+    flag: '/assets/images/gr.svg',
+    code: '+30',
+  },
+  {
+    id: 'GL',
+    flag: '/assets/images/gl.svg',
+    code: '+299',
+  },
+  {
+    id: 'GD',
+    flag: '/assets/images/gd.svg',
+    code: '+1',
+  },
+  {
+    id: 'GP',
+    flag: '/assets/images/gp.svg',
+    code: '+590',
+  },
+  {
+    id: 'GT',
+    flag: '/assets/images/gt.svg',
+    code: '+502',
+  },
+  {
+    id: 'GG',
+    flag: '/assets/images/gg.svg',
+    code: '+44',
+  },
+  {
+    id: 'GN',
+    flag: '/assets/images/gn.svg',
+    code: '+224',
+  },
+  {
+    id: 'GW',
+    flag: '/assets/images/gw.svg',
+    code: '+245',
+  },
+  {
+    id: 'GY',
+    flag: '/assets/images/gy.svg',
+    code: '+592',
+  },
+  {
+    id: 'HT',
+    flag: '/assets/images/ht.svg',
+    code: '+509',
+  },
+  {
+    id: 'HN',
+    flag: '/assets/images/hn.svg',
+    code: '+504',
+  },
+  {
+    id: 'HK',
+    flag: '/assets/images/hk.svg',
+    code: '+852',
+  },
+  {
+    id: 'HU',
+    flag: '/assets/images/hu.svg',
+    code: '+36',
+  },
+  {
+    id: 'IS',
+    flag: '/assets/images/is.svg',
+    code: '+354',
+  },
+  {
+    id: 'IN',
+    flag: '/assets/images/in.svg',
+    code: '+91',
+  },
+  {
+    id: 'ID',
+    flag: '/assets/images/id.svg',
+    code: '+62',
+  },
+  {
+    id: 'IQ',
+    flag: '/assets/images/iq.svg',
+    code: '+964',
+  },
+  {
+    id: 'IE',
+    flag: '/assets/images/ie.svg',
+    code: '+353',
+  },
+  {
+    id: 'IM',
+    flag: '/assets/images/im.svg',
+    code: '+44',
+  },
+  {
+    id: 'IL',
+    flag: '/assets/images/il.svg',
+    code: '+972',
+  },
+  {
+    id: 'IT',
+    flag: '/assets/images/it.svg',
+    code: '+39',
+  },
+  {
+    id: 'JM',
+    flag: '/assets/images/jm.svg',
+    code: '+1',
+  },
+  {
+    id: 'JP',
+    flag: '/assets/images/jp.svg',
+    code: '+81',
+  },
+  {
+    id: 'JE',
+    flag: '/assets/images/je.svg',
+    code: '+44',
+  },
+  {
+    id: 'JO',
+    flag: '/assets/images/jo.svg',
+    code: '+962',
+  },
+  {
+    id: 'KZ',
+    flag: '/assets/images/kz.svg',
+    code: '+7',
+  },
+  {
+    id: 'KE',
+    flag: '/assets/images/ke.svg',
+    code: '+254',
+  },
+  {
+    id: 'KI',
+    flag: '/assets/images/ki.svg',
+    code: '+686',
+  },
+  {
+    id: 'XK',
+    flag: '/assets/images/xk.svg',
+    code: '+383',
+  },
+  {
+    id: 'KW',
+    flag: '/assets/images/kw.svg',
+    code: '+965',
+  },
+  {
+    id: 'KG',
+    flag: '/assets/images/kg.svg',
+    code: '+996',
+  },
+  {
+    id: 'LA',
+    flag: '/assets/images/la.svg',
+    code: '+856',
+  },
+  {
+    id: 'LV',
+    flag: '/assets/images/lv.svg',
+    code: '+371',
+  },
+  {
+    id: 'LB',
+    flag: '/assets/images/lb.svg',
+    code: '+961',
+  },
+  {
+    id: 'LS',
+    flag: '/assets/images/ls.svg',
+    code: '+266',
+  },
+  {
+    id: 'LR',
+    flag: '/assets/images/lr.svg',
+    code: '+231',
+  },
+  {
+    id: 'LY',
+    flag: '/assets/images/ly.svg',
+    code: '+218',
+  },
+  {
+    id: 'LI',
+    flag: '/assets/images/li.svg',
+    code: '+423',
+  },
+  {
+    id: 'LT',
+    flag: '/assets/images/lt.svg',
+    code: '+370',
+  },
+  {
+    id: 'LU',
+    flag: '/assets/images/lu.svg',
+    code: '+352',
+  },
+  {
+    id: 'MO',
+    flag: '/assets/images/mo.svg',
+    code: '+853',
+  },
+  {
+    id: 'MG',
+    flag: '/assets/images/mg.svg',
+    code: '+261',
+  },
+  {
+    id: 'MW',
+    flag: '/assets/images/mw.svg',
+    code: '+265',
+  },
+  {
+    id: 'MY',
+    flag: '/assets/images/my.svg',
+    code: '+60',
+  },
+  {
+    id: 'MV',
+    flag: '/assets/images/mv.svg',
+    code: '+960',
+  },
+  {
+    id: 'ML',
+    flag: '/assets/images/ml.svg',
+    code: '+223',
+  },
+  {
+    id: 'MT',
+    flag: '/assets/images/mt.svg',
+    code: '+356',
+  },
+  {
+    id: 'MQ',
+    flag: '/assets/images/mq.svg',
+    code: '+596',
+  },
+  {
+    id: 'MR',
+    flag: '/assets/images/mr.svg',
+    code: '+222',
+  },
+  {
+    id: 'MU',
+    flag: '/assets/images/mu.svg',
+    code: '+230',
+  },
+  {
+    id: 'YT',
+    flag: '/assets/images/Mayotte-Flag.svg',
+    code: '+262',
+  },
+  {
+    id: 'MX',
+    flag: '/assets/images/mx.svg',
+    code: '+52',
+  },
+  {
+    id: 'MD',
+    flag: '/assets/images/md.svg',
+    code: '+373',
+  },
+  {
+    id: 'MC',
+    flag: '/assets/images/mc.svg',
+    code: '+377',
+  },
+  {
+    id: 'MN',
+    flag: '/assets/images/mn.svg',
+    code: '+976',
+  },
+  {
+    id: 'ME',
+    flag: '/assets/images/me.svg',
+    code: '+382',
+  },
+  {
+    id: 'MS',
+    flag: '/assets/images/ms.svg',
+    code: '+1',
+  },
+  {
+    id: 'MA',
+    flag: '/assets/images/ma.svg',
+    code: '+212',
+  },
+  {
+    id: 'MZ',
+    flag: '/assets/images/mz.svg',
+    code: '+258',
+  },
+  {
+    id: 'MM',
+    flag: '/assets/images/mm.svg',
+    code: '+95',
+  },
+  {
+    id: 'NA',
+    flag: '/assets/images/na.svg',
+    code: '+264',
+  },
+  {
+    id: 'NR',
+    flag: '/assets/images/nr.svg',
+    code: '+674',
+  },
+  {
+    id: 'NP',
+    flag: '/assets/images/np.svg',
+    code: '+977',
+  },
+  {
+    id: 'NL',
+    flag: '/assets/images/nl.svg',
+    code: '+31',
+  },
+  {
+    id: 'NC',
+    flag: '/assets/images/bl.svg',
+    code: '+687',
+  },
+  {
+    id: 'NZ',
+    flag: '/assets/images/nz.svg',
+    code: '+64',
+  },
+  {
+    id: 'NI',
+    flag: '/assets/images/ni.svg',
+    code: '+505',
+  },
+  {
+    id: 'NE',
+    flag: '/assets/images/ne.svg',
+    code: '+227',
+  },
+  {
+    id: 'NG',
+    flag: '/assets/images/ng.svg',
+    code: '+234',
+  },
+  {
+    id: 'NU',
+    flag: '/assets/images/nu.svg',
+    code: '+683',
+  },
+  {
+    id: 'NF',
+    flag: '/assets/images/nf.svg',
+    code: '+672',
+  },
+  {
+    id: 'MK',
+    flag: '/assets/images/mk.svg',
+    code: '+389',
+  },
+  {
+    id: 'NO',
+    flag: '/assets/images/no.svg',
+    code: '+47',
+  },
+  {
+    id: 'OM',
+    flag: '/assets/images/om.svg',
+    code: '+968',
+  },
+  {
+    id: 'PK',
+    flag: '/assets/images/pk.svg',
+    code: '+92',
+  },
+  {
+    id: 'PS',
+    flag: '/assets/images/ps.svg',
+    code: '+970',
+  },
+  {
+    id: 'PA',
+    flag: '/assets/images/pa.svg',
+    code: '+507',
+  },
+  {
+    id: 'PG',
+    flag: '/assets/images/pg.svg',
+    code: '+675',
+  },
+  {
+    id: 'PY',
+    flag: '/assets/images/py.svg',
+    code: '+595',
+  },
+  {
+    id: 'PE',
+    flag: '/assets/images/pe.svg',
+    code: '+51',
+  },
+  {
+    id: 'PH',
+    flag: '/assets/images/ph.svg',
+    code: '+63',
+  },
+  {
+    id: 'PN',
+    flag: '/assets/images/pn.svg',
+    code: '+64',
+  },
+  {
+    id: 'PL',
+    flag: '/assets/images/pl.svg',
+    code: '+48',
+  },
+  {
+    id: 'PT',
+    flag: '/assets/images/pt.svg',
+    code: '+351',
+  },
+  {
+    id: 'QA',
+    flag: '/assets/images/qa.svg',
+    code: '+974',
+  },
+  {
+    id: 'RE',
+    flag: '/assets/images/bl.svg',
+    code: '+262',
+  },
+  {
+    id: 'RO',
+    flag: '/assets/images/ro.svg',
+    code: '+40',
+  },
+  {
+    id: 'RU',
+    flag: '/assets/images/ru.svg',
+    code: '+7',
+  },
+  {
+    id: 'RW',
+    flag: '/assets/images/rw.svg',
+    code: '+250',
+  },
+  {
+    id: 'WS',
+    flag: '/assets/images/ws.svg',
+    code: '+685',
+  },
+  {
+    id: 'SM',
+    flag: '/assets/images/sm.svg',
+    code: '+378',
+  },
+  {
+    id: 'ST',
+    flag: '/assets/images/st.svg',
+    code: '+239',
+  },
+  {
+    id: 'SA',
+    flag: '/assets/images/sa.svg',
+    code: '+966',
+  },
+  {
+    id: 'SN',
+    flag: '/assets/images/sn.svg',
+    code: '+221',
+  },
+  {
+    id: 'RS',
+    flag: '/assets/images/rs.svg',
+    code: '+381',
+  },
+  {
+    id: 'SC',
+    flag: '/assets/images/sc.svg',
+    code: '+248',
+  },
+  {
+    id: 'SL',
+    flag: '/assets/images/sl.svg',
+    code: '+232',
+  },
+  {
+    id: 'SG',
+    flag: '/assets/images/sg.svg',
+    code: '+65',
+  },
+  {
+    id: 'SX',
+    flag: '/assets/images/sx.svg',
+    code: '+1',
+  },
+  {
+    id: 'SK',
+    flag: '/assets/images/sk.svg',
+    code: '+421',
+  },
+  {
+    id: 'SI',
+    flag: '/assets/images/si.svg',
+    code: '+386',
+  },
+  {
+    id: 'SB',
+    flag: '/assets/images/sb.svg',
+    code: '+677',
+  },
+  {
+    id: 'SO',
+    flag: '/assets/images/so.svg',
+    code: '+252',
+  },
+  {
+    id: 'ZA',
+    flag: '/assets/images/za.svg',
+    code: '+27',
+  },
+  {
+    id: 'GS',
+    flag: '/assets/images/gs-2.svg',
+    code: '+500',
+  },
+  {
+    id: 'KR',
+    flag: '/assets/images/kr.svg',
+    code: '+82',
+  },
+  {
+    id: 'SS',
+    flag: '/assets/images/ss.svg',
+    code: '+211',
+  },
+  {
+    id: 'ES',
+    flag: '/assets/images/es.svg',
+    code: '+34',
+  },
+  {
+    id: 'LK',
+    flag: '/assets/images/lk.svg',
+    code: '+94',
+  },
+  {
+    id: 'BL',
+    flag: '/assets/images/flag-for-flag-st-barthelemy.svg',
+    code: '+590',
+  },
+  {
+    id: 'SH',
+    flag: '/assets/images/sh.svg',
+    code: '+290',
+  },
+  {
+    id: 'KN',
+    flag: '/assets/images/kn.svg',
+    code: '+1',
+  },
+  {
+    id: 'LC',
+    flag: '/assets/images/lc.svg',
+    code: '+1',
+  },
+  {
+    id: 'MF',
+    flag: '/assets/images/mf.svg',
+    code: '+590',
+  },
+  {
+    id: 'PM',
+    flag: '/assets/images/pm.svg',
+    code: '+508',
+  },
+  {
+    id: 'VC',
+    flag: '/assets/images/vc.svg',
+    code: '+1',
+  },
+  {
+    id: 'SD',
+    flag: '/assets/images/sd.svg',
+    code: '+249',
+  },
+  {
+    id: 'SR',
+    flag: '/assets/images/sr.svg',
+    code: '+597',
+  },
+  {
+    id: 'SJ',
+    flag: '/assets/images/no.svg',
+    code: '+47',
+  },
+  {
+    id: 'SE',
+    flag: '/assets/images/se.svg',
+    code: '+46',
+  },
+  {
+    id: 'CH',
+    flag: '/assets/images/ch.svg',
+    code: '+41',
+  },
+  {
+    id: 'TW',
+    flag: '/assets/images/tw.svg',
+    code: '+886',
+  },
+  {
+    id: 'TJ',
+    flag: '/assets/images/tj.svg',
+    code: '+992',
+  },
+  {
+    id: 'TZ',
+    flag: '/assets/images/tz.svg',
+    code: '+255',
+  },
+  {
+    id: 'TH',
+    flag: '/assets/images/th.svg',
+    code: '+66',
+  },
+  {
+    id: 'TL',
+    flag: '/assets/images/tl.svg',
+    code: '+670',
+  },
+  {
+    id: 'TG',
+    flag: '/assets/images/tg.svg',
+    code: '+228',
+  },
+  {
+    id: 'TK',
+    flag: '/assets/images/tk.svg',
+    code: '+690',
+  },
+  {
+    id: 'TO',
+    flag: '/assets/images/to.svg',
+    code: '+676',
+  },
+  {
+    id: 'TT',
+    flag: '/assets/images/tt.svg',
+    code: '+1',
+  },
+  {
+    id: 'TA',
+    flag: '/assets/images/ta.svg',
+    code: '+2908',
+  },
+  {
+    id: 'TN',
+    flag: '/assets/images/tn.svg',
+    code: '+216',
+  },
+  {
+    id: 'TR',
+    flag: '/assets/images/tr.svg',
+    code: '+90',
+  },
+  {
+    id: 'TM',
+    flag: '/assets/images/tm.svg',
+    code: '+993',
+  },
+  {
+    id: 'TC',
+    flag: '/assets/images/tc.svg',
+    code: '+1',
+  },
+  {
+    id: 'TV',
+    flag: '/assets/images/tv.svg',
+    code: '+688',
+  },
+  {
+    id: 'UM',
+    flag: '/assets/images/us-flag.svg',
+    code: '+1',
+  },
+  {
+    id: 'UG',
+    flag: '/assets/images/ug.svg',
+    code: '+256',
+  },
+  {
+    id: 'UA',
+    flag: '/assets/images/ua.svg',
+    code: '+380',
+  },
+  {
+    id: 'AE',
+    flag: '/assets/images/ae.svg',
+    code: '+971',
+  },
+  {
+    id: 'GB',
+    flag: '/assets/images/gb.svg',
+    code: '+44',
+  },
+  {
+    id: 'UY',
+    flag: '/assets/images/uy.svg',
+    code: '+598',
+  },
+  {
+    id: 'UZ',
+    flag: '/assets/images/uz.svg',
+    code: '+998',
+  },
+  {
+    id: 'VU',
+    flag: '/assets/images/vu.svg',
+    code: '+678',
+  },
+  {
+    id: 'VA',
+    flag: '/assets/images/Flag_of_Vatican_City.svg',
+    code: '+39',
+  },
+  {
+    id: 'VE',
+    flag: '/assets/images/ve.svg',
+    code: '+58',
+  },
+  {
+    id: 'VN',
+    flag: '/assets/images/vn.svg',
+    code: '+84',
+  },
+  {
+    id: 'WF',
+    flag: '/assets/images/fr.svg',
+    code: '+681',
+  },
+  {
+    id: 'EH',
+    flag: '/assets/images/ma.svg',
+    code: '+212',
+  },
+  {
+    id: 'YE',
+    flag: '/assets/images/ye.svg',
+    code: '+967',
+  },
+  {
+    id: 'ZM',
+    flag: '/assets/images/zm.svg',
+    code: '+260',
+  },
+  {
+    id: 'ZW',
+    flag: '/assets/images/zw.svg',
+    code: '+263',
+  },
+]
