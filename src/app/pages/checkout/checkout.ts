2496ab9ebab3c58522b3fe2241dd23bc7e149db0
migel <migelangelo10@gmail.com>
2025-12-27 10:33:13 +0200

    credit card completed

--- src/app/pages/checkout/checkout.ts
+++ src/app/pages/checkout/checkout.ts
@@ -21,37 +21,84 @@ export class Checkout {
   public cartItems: ProductType[] = [];
   public labelActive: boolean = false;
   public labelFirstNameActive: boolean = false;
+  public labelBillingFirstNameActive: boolean = false;
   public labelLastNameActive: boolean = false;
+  public labelBillingLastNameActive: boolean = false;
   public labelCompanyActive: boolean = false;
+  public labelBillingCompanyActive: boolean = false;
   public labelShippingAddressActive: boolean = false;
+  public labelBillingAddressActive: boolean = false;
   public labelApartmentActive: boolean = false;
+  public labelBillingApartmentActive: boolean = false;
   public labelCityActive: boolean = false;
+  public labelBillingCityActive: boolean = false;
   public labelZipcodeActive: boolean = false;
+  public labelBillingZipcodeActive: boolean = false;
   public labelPostalcodeActive: boolean = false;
+  public labelBillingPostalcodeActive: boolean = false;
   public labelPhoneActive: boolean = false;
+  public labelBillingPhoneActive: boolean = false;
+  public labelSmsActive: boolean = false;
+  public labelCardNumberActive: boolean = false;
+  public labelExpirationDateActive: boolean = false;
+  public labelCvvActive: boolean = false;
+  public labelCardNameActive: boolean = false;
   public shipToAddressChecked: boolean = true;
   public shipToPickupPointChecked: boolean = false;
   public currentCountry: string = 'US';
+  public currentBillingCountry: string = 'US';
 
   public phones: PhoneType[] = Phones;
   public currentPhoneFlag: string = this.phones[0].flag;
+  public currentBillingPhoneFlag: string = this.phones[0].flag;
+  public userPhone: string = '';
+  public userBillingPhone: string = '';
+  public phoneCodeDetected: boolean = false;
+  public phoneBillingCodeDetected: boolean = false;
+  public sendSmsMessage: boolean = false;
+  public useShippingAddressAsBillingAddress: boolean = false;
+  public processing: boolean = false;
+  public locationAllowed: boolean = true;
+
+  public basicCardChecked: boolean = true;
 
   public formCheckoutGroup: FormGroup = new FormGroup({
     email: new FormControl('', [Validators.email]),
     checkBox: new FormControl(true),
     delivery_strategies: new FormControl('delivery-to-address'),
     country_select: new FormControl('US'),
+    billing_country_select: new FormControl('US'),
     first_name: new FormControl('', [Validators.required]),
+    billing_first_name: new FormControl('', [Validators.required]),
     last_name: new FormControl('', [Validators.required]),
+    billing_last_name: new FormControl('', [Validators.required]),
     company: new FormControl(''),
+    billing_company: new FormControl(''),
     shipping_address: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    billing_address: new FormControl('', [Validators.required, Validators.minLength(6)]),
     apartment: new FormControl(''),
+    billing_apartment: new FormControl(''),
     city: new FormControl('', [Validators.required]),
+    billing_city: new FormControl('', [Validators.required]),
     state_select: new FormControl(''),
+    billing_state_select: new FormControl(''),
     zip_code: new FormControl('', [Validators.required]),
+    billing_zip_code: new FormControl('', [Validators.required]),
     postal_code: new FormControl(''),
+    billing_postal_code: new FormControl(''),
     phone: new FormControl('', [Validators.required, CustomValidators.phoneValidator]),
+    billing_phone: new FormControl('', [Validators.required, CustomValidators.phoneValidator]),
+    phoneSms: new FormControl('', [Validators.required, CustomValidators.phoneValidator]),
     phone_country_select: new FormControl(''),
+    billing_phone_country_select: new FormControl(''),
+    phoneSms_country_select: new FormControl(''),
+    checkBoxTextMe: new FormControl(false),
+    paymentMethod: new FormControl('Basic card'),
+    credit_card_number: new FormControl('', [Validators.required, Validators.minLength(12), Validators.pattern('^[0-9]*$')]),
+    expiry: new FormControl('', [Validators.required]),
+    cvv: new FormControl('', [Validators.required]),
+    card_name: new FormControl('', [Validators.required]),
+    checkBoxUseBillingAddress: new FormControl(true),
   });
 
   constructor(private  cartService: AddToCart, private scrollTotopService: ScrollTotopService) {
@@ -66,11 +113,22 @@ export class Checkout {
 
   get email() { return this.formCheckoutGroup.get('email'); }
   get firstName() { return this.formCheckoutGroup.get('first_name'); }
+  get billingFirstName() { return this.formCheckoutGroup.get('billing_first_name'); }
   get lastName() { return this.formCheckoutGroup.get('last_name'); }
+  get billingLastName() { return this.formCheckoutGroup.get('billing_last_name'); }
   get shippingAddress() { return this.formCheckoutGroup.get('shipping_address'); }
+  get billingAddress() { return this.formCheckoutGroup.get('billing_address'); }
   get city() { return this.formCheckoutGroup.get('city'); }
+  get billingCity() { return this.formCheckoutGroup.get('billing_city'); }
   get zipcode() { return this.formCheckoutGroup.get('zip_code'); }
+  get billingZipcode() { return this.formCheckoutGroup.get('billing_zip_code'); }
   get phone() { return this.formCheckoutGroup.get('phone'); }
+  get billingPhone() { return this.formCheckoutGroup.get('billing_phone'); }
+  get phoneSms() { return this.formCheckoutGroup.get('phoneSms'); }
+  get creditCardNumber() { return this.formCheckoutGroup.get('credit_card_number'); }
+  get expirationDate() { return this.formCheckoutGroup.get('expiry'); }
+  get cvv() { return this.formCheckoutGroup.get('cvv'); }
+  get cardName() { return this.formCheckoutGroup.get('card_name'); }
 
   public onInputChange(): void {
     this.labelActive = this.formCheckoutGroup.value.email.length > 0;
@@ -78,32 +136,128 @@ export class Checkout {
   public onInputCountryChange(): void {
     this.currentCountry = this.formCheckoutGroup.value.country_select;
   }
+  public onInputBillingCountryChange(): void {
+    this.currentBillingCountry = this.formCheckoutGroup.value.billing_country_select;
+  }
   public onInputFirstNameChange(): void {
     this.labelFirstNameActive = this.formCheckoutGroup.value.first_name.length > 0;
+  }
+  public onInputBillingFirstNameChange(): void {
+    this.labelBillingFirstNameActive = this.formCheckoutGroup.value.billing_first_name.length > 0;
   }
   public onInputLastNameChange(): void {
     this.labelLastNameActive = this.formCheckoutGroup.value.last_name.length > 0;
   }
+  public onInputBillingLastNameChange(): void {
+    this.labelBillingLastNameActive = this.formCheckoutGroup.value.billing_last_name.length > 0;
+  }
   public onInputCompanyChange(): void {
     this.labelCompanyActive = this.formCheckoutGroup.value.company.length > 0;
+  }
+  public onInputBillingCompanyChange(): void {
+    this.labelBillingCompanyActive = this.formCheckoutGroup.value.billing_company.length > 0;
   }
   public onInputShippingAddressChange(): void {
     this.labelShippingAddressActive = this.formCheckoutGroup.value.shipping_address.length > 0;
   }
+  public onInputBillingAddressChange(): void {
+    this.labelBillingAddressActive = this.formCheckoutGroup.value.billing_address.length > 0;
+  }
   public onInputShippingApartmentChange(): void {
     this.labelApartmentActive = this.formCheckoutGroup.value.apartment.length > 0;
+  }
+  public onInputBillingApartmentChange(): void {
+    this.labelBillingApartmentActive = this.formCheckoutGroup.value.billing_apartment.length > 0;
   }
   public onInputShippingCityChange(): void {
     this.labelCityActive = this.formCheckoutGroup.value.city.length > 0;
   }
+  public onInputBillingCityChange(): void {
+    this.labelBillingCityActive = this.formCheckoutGroup.value.billing_city.length > 0;
+  }
   public onInputZipcodeChange(): void {
     this.labelZipcodeActive = this.formCheckoutGroup.value.zip_code.length > 0;
+  }
+  public onInputBillingZipcodeChange(): void {
+    this.labelBillingZipcodeActive = this.formCheckoutGroup.value.billing_zip_code.length > 0;
   }
   public onInputPostalodeChange(): void {
     this.labelPostalcodeActive = this.formCheckoutGroup.value.postal_code.length > 0;
   }
+  public onInputBillingPostalodeChange(): void {
+    this.labelBillingPostalcodeActive = this.formCheckoutGroup.value.billing_postal_code.length > 0;
+  }
   public onInputPhoneChange(): void {
     this.labelPhoneActive = this.formCheckoutGroup.value.phone.length > 0;
+
+    if (this.formCheckoutGroup.value.phone.length > 0) {
+      this.userPhone = this.formCheckoutGroup.value.phone;
+      this.checkUserPhone();
+      this.formCheckoutGroup.patchValue({
+        phoneSms: this.userPhone
+      });
+      this.labelSmsActive = true;
+    }
+  }
+  public onInputBillingPhoneChange(): void {
+    this.labelBillingPhoneActive = this.formCheckoutGroup.value.billing_phone.length > 0;
+
+    if (this.formCheckoutGroup.value.billing_phone.length > 0) {
+      this.userBillingPhone = this.formCheckoutGroup.value.billing_phone;
+      this.checkBillingUserPhone();
+    }
+  }
+  public onInputMobilePhoneChange(): void {
+    this.labelSmsActive = this.formCheckoutGroup.value.phoneSms.length > 0;
+
+    if (this.formCheckoutGroup.value.phoneSms.length > 0) {
+      this.userPhone = this.formCheckoutGroup.value.phoneSms;
+      this.checkUserPhone();
+      this.formCheckoutGroup.patchValue({
+        phone: this.userPhone
+      });
+    }
+  }
+  public onInputCreditCardNumberChange(): void {
+    this.labelCardNumberActive = this.formCheckoutGroup.value.credit_card_number.length > 0;
+  }
+  public onInputExpireChange(): void {
+    this.labelExpirationDateActive = this.formCheckoutGroup.value.expiry.length > 0;
+  }
+  public onInputCvvChange(): void {
+    this.labelCvvActive = this.formCheckoutGroup.value.cvv.length > 0;
+  }
+  public onInputCardNameChange(): void {
+    this.labelCardNameActive = this.formCheckoutGroup.value.card_name.length > 0;
+  }
+  private checkUserPhone(): void {
+    this.phoneCodeDetected = false;
+
+    for (let i:number = 0; i <= this.phones.length; i++) {
+      if (this.userPhone.includes(this.phones[i].code)) {
+        this.currentPhoneFlag = this.phones[i].flag;
+        this.phoneCodeDetected = true;
+        this.formCheckoutGroup.patchValue({
+          phone_country_select: this.phones[i].id,
+          phoneSms_country_select: this.phones[i].id
+        });
+        break;
+      }
+    }
+  }
+  private checkBillingUserPhone(): void {
+    this.phoneBillingCodeDetected = false;
+
+    for (let i:number = 0; i <= this.phones.length; i++) {
+      if (this.userBillingPhone.includes(this.phones[i].code)) {
+        this.currentBillingPhoneFlag = this.phones[i].flag;
+        this.phoneBillingCodeDetected = true;
+        this.formCheckoutGroup.patchValue({
+          billing_phone_country_select: this.phones[i].id,
+        });
+        break;
+      }
+    }
   }
   public onInputPhoneCountryChange(): void {
 
@@ -114,13 +268,37 @@ export class Checkout {
       let value: string = code + " ";
 
       this.formCheckoutGroup.patchValue({
-        phone: value
+        phone: value,
+        phoneSms: value
       });
     }
 
     if (flag !== null) {
       this.currentPhoneFlag = flag;
     }
+  }
+  public onInputBillingPhoneCountryChange(): void {
+
+    let code: string | null = this.getPhoneCode(this.formCheckoutGroup.value.billing_phone_country_select);
+    let flag: string | null = this.getPhoneFlag(this.formCheckoutGroup.value.billing_phone_country_select);
+
+    if (code !== null) {
+      let value: string = code + " ";
+
+      this.formCheckoutGroup.patchValue({
+        billing_phone: value,
+      });
+    }
+
+    if (flag !== null) {
+      this.currentBillingPhoneFlag = flag;
+    }
+  }
+  public onInputTextMeChange(): void {
+    this.sendSmsMessage = this.formCheckoutGroup.value.checkBoxTextMe;
+  }
+  public onInputUseShippingAddressChange(): void {
+    this.useShippingAddressAsBillingAddress = this.formCheckoutGroup.value.checkBoxUseBillingAddress;
   }
 
   public shipToAddressClick(): void {
@@ -148,5 +326,60 @@ export class Checkout {
       }
     }
     return null;
+  }
+  public useMyLocation(): void {
+    this.processing = true;
+
+    setTimeout(() => {
+      this.locationAllowed = confirm("Allow Vintina to access your location?");
+      if (this.locationAllowed) {
+
+      }
+      this.processing = false;
+    }, 500);
+  }
+  public optionBasicCardClick(): void {
+    this.basicCardChecked = true;
+  }
+  public numericInputOnly(e: KeyboardEvent): void {
+    if (e.code.includes('Digit') || e.code.includes('Backspace')) {
+      return;
+    }
+    e.preventDefault();
+  }
+  public numericInput(e: KeyboardEvent): void {
+
+    this.labelExpirationDateActive = true;
+
+    if (e.code.includes('Digit') || e.code.includes('Backspace') || e.code.includes('Slash')
+    || e.code.includes('NumpadDivide')) {
+
+      if (e.code.includes('Digit')) {
+        let digit = parseInt(e.code.split('Digit')[1]);
+
+        if (digit === 1 && this.formCheckoutGroup.value.expiry.length === 0) {
+          return;
+        }
+        if (digit > 1 && this.formCheckoutGroup.value.expiry.length === 0) {
+          this.formCheckoutGroup.patchValue({
+            expiry: `0${digit}/`
+          });
+          e.preventDefault();
+          return;
+        }
+        if (this.formCheckoutGroup.value.expiry.length === 1) {
+          this.formCheckoutGroup.patchValue({
+            expiry: this.formCheckoutGroup.value.expiry + `${digit}/`
+          });
+          e.preventDefault();
+          return;
+        }
+        if (this.formCheckoutGroup.value.expiry.length > 6) {
+          e.preventDefault();
+        }
+      }
+      return;
+    }
+    e.preventDefault();
   }
 }
